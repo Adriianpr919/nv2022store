0368641eb820208639c590ab3fadc020f226f8f8
LINO ADRIÁN PÁEZ REYES <54821048+Adriianpr919@users.noreply.github.com>
2022-12-19 14:50:32 -0500

    update

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,40 @@
-import Head from 'next/head'
-import React from 'react'
+import Head from 'next/head';
+{/**/ } import Link from 'next/link';
+{/**/ } import Image from 'next/image';
+import React, { useState } from 'react';
+import Logo from '../public/Logonv.png';
+import FooterDiv from './Footer';
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+{/* import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe, faBagShopping } from '@fortawesome/free-solid-svg-icons';*/ }
+
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBBadge,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+  MDBInputGroup
+} from 'mdb-react-ui-kit';
 
 export default function Layout({ title, children }) {
+  const [showBasic, setShowBasic] = useState(false);
   return (
     <>
       <Head>
-        <title data-rh="true">Nury Valenzuela.</title>
-        <meta charset="UTF-8" />
+        <title data-rh="true">{title ? title + ' - Nury Valenzuela' : 'Nury Valenzuela'}</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="format-detection" content="telephone=no" />
@@ -19,17 +47,96 @@ export default function Layout({ title, children }) {
         <link rel="apple-touch-icon" href="/logo.ico" />
         <meta name="theme-color" content="#E6ACA9" />
       </Head>
-      <div>
+      <div className="flex min-h-screen flex-col justify-between ">
         <header>
+          <MDBNavbar expand='lg' light bgColor='light' className="flex items-center px-12 justify-between shadow-md">
+            <MDBContainer fluid>
+              <MDBNavbarBrand href="/" rel="noopener noreferrer">
+                <Image
+                  src={Logo}
+                  className="site_logo"
+                  alt="Nury Valenzuela."
+                  title="Nury Valenzuela."
+                  loading="lazy"
+                  lang="es" />
+              </MDBNavbarBrand>
 
+              <MDBNavbarToggler
+                aria-controls='navbarSupportedContent'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+                onClick={() => setShowBasic(!showBasic)}
+              >
+                <MDBIcon icon='bars' fas />
+              </MDBNavbarToggler>
+
+              <MDBCollapse navbar show={showBasic}>
+                <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+                  <MDBNavbarItem>
+                    <MDBNavbarLink active aria-current='page' className="text-lg font-bold" href='/' rel="noopener noreferrer">
+                      <MDBIcon fas icon="globe" /> INICIO.
+                    </MDBNavbarLink>
+                  </MDBNavbarItem>
+                  <MDBNavbarItem>
+                    <MDBDropdown>
+                      <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                        <MDBIcon fas icon="shopping-bag" /> Ver Productos.
+                      </MDBDropdownToggle>
+                      <MDBDropdownMenu>
+                        <MDBDropdownItem className="text-lg font-bold" link>Compras Por Categorías.</MDBDropdownItem>
+                        <MDBDropdownItem divider />
+                        <MDBDropdownItem link>NUEVOS.</MDBDropdownItem>
+                        <MDBDropdownItem link>DESTACADOS.</MDBDropdownItem>
+                      </MDBDropdownMenu>
+                    </MDBDropdown>
+                  </MDBNavbarItem>
+                  <MDBNavbarItem>
+                    <MDBInputGroup tag="form" className='d-flex w-auto mb-3'>
+                      <input className='form-control' placeholder="BUSCAR" aria-label="BUSCAR" type='BUSCAR' />
+                      <MDBBtn outline><MDBIcon fas icon="search" /> BUSCAR.</MDBBtn>
+                    </MDBInputGroup>
+                  </MDBNavbarItem>
+                </MDBNavbarNav>
+
+                <Link className='mx-3' href='/login' rel="noopener noreferrer">
+                  <MDBIcon fas icon='users' style={{ fontSize: "20px", color: "black" }} size='lg' />
+                  <MDBBadge color='danger' notification pill>
+                    1
+                  </MDBBadge>
+                </Link>
+                <Link className='mx-3' href='/cart' rel="noopener noreferrer">
+                  <MDBIcon fas icon='shopping-cart' style={{ fontSize: "20px", color: "black" }} size='lg' />
+                  <MDBBadge color='danger' notification pill>
+                    1
+                  </MDBBadge>
+                </Link>
+              </MDBCollapse>
+            </MDBContainer>
+          </MDBNavbar>
+          <div
+            className='p-5 text-center bg-image'
+            style={{ backgroundImage: "url('https://mdbootstrap.com/img/new/slides/041.webp')", height: '400px' }}
+          >
+            <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+              <div className='d-flex justify-content-center align-items-center h-100'>
+                <div className='text-white'>
+                  <h1 className='mb-3'>Heading</h1>
+                  <h4 className='mb-3'>Subheading</h4>
+                  <MDBBtn tag="a" outline size="lg">
+                    Call to action
+                  </MDBBtn>
+                </div>
+              </div>
+            </div>
+          </div>
         </header>
-        <main>
+        <main className="container m-auto mt-4 px-4">
           {children}
         </main>
-        <footer>
-
+        <footer className="text-center text-lg-start bg-light text-muted shadow-inner">
+          <FooterDiv />
         </footer>
       </div>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import Head from 'next/head'
+
+import Layout from '../components/Layout';
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 export default function Home() {
   return (
-    <div>
+    <Layout title="Home Page">
 
 
       <h2 className="h2 my-4">PRODUCTOS DESTACADOS.</h2>
-    </div>
-  )
+    </Layout>
+  );
 }
