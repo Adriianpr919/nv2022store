8418d2b634339075658134100685a196f7be0945
LINO ADRIÁN PÁEZ REYES <54821048+Adriianpr919@users.noreply.github.com>
2022-12-19 16:43:58 -0500

    update.

---
+++ components/ProductItem.js
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import Link from 'next/link';
+
+export default function ProductItem({ product }) {
+  return (
+    <>
+      <div className="card text-center border border-primary shadow-0 ">
+        <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+          <Link href={`/product/${product.slug}`}>
+            <img src={product.image} className="img-fluid rounded shadow object-cover h-64 w-full" alt={product.name} title={product.name} />
+          </Link>
+        </div>
+        <div className="">
+          <div className="card-header">{product.brand}</div>
+          <div className="card-body">
+            <h5 className="card-title">
+              <Link href={`/product/${product.slug}`}>
+                <h5 className="text-lg">{product.name}</h5>
+              </Link>
+            </h5>
+            <p className="card-text">
+              $ {product.price} - COP
+            </p>
+          </div>
+          <div className="card-footer">
+            <button type="button" className="btn btn-primary">
+              <i className="fa fa-shopping-basket" /> Añadir Al Carrito.
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 
+import data from '../utils/data'
 import Layout from '../components/Layout';
 
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import ProductItem from '../components/ProductItem';
 
 export default function Home() {
   return (
     <Layout title="Home Page">
-
-
       <h2 className="h2 my-4">PRODUCTOS DESTACADOS.</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem
+          product={product}
+          key={product.slug}
+        ></ProductItem>
+        ))}
+      </div>
     </Layout>
   );
 }
